c4bfa3807726313ac3c1ac0e4051a839feca1ae4
EowynStark <andwellm89@gmail.com>
2023-11-03 09:48:29 -0500

    adding pseudocode to script file

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,11 @@
 // Assignment code here
+// generate a password on a click
+// series of prompts for password criteria
+// prompt for length of password (no less than 8 characters and no more than 128)
+// prompt for character types (uppercase, lowercase, numeric, special characters)
+// on an answer for each prompt answers are validated and at least one character chosen
+// when all prompts are answered then a password is generated
+// generated password is either shown by alert or written onto the page 
 
 
 // Get references to the #generate element
